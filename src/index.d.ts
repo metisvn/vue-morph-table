d3c7e9dd45cec83aac2e86f5ae8922fa8824cec6
Cao Chinh Bùi <buicaochinh0811@gmail.com>
2021-03-08 16:07:34 +0700

    type definition

---
+++ src/index.d.ts
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+
+export declare class VueMorphTable extends Vue {
+  items?: Array<any>
+  fields?: Array<any>
+  addTableClasses?: string | Array<any> | object
+  responsive?: boolean
+  size?: string
+  dark?: boolean
+  striped?: boolean
+  fixed?: Boolean
+  hover?: Boolean
+  border?: Boolean
+  outlined?: Boolean
+  header?: boolean
+  sorter?: boolean
+  sorterValue?: { column: string, asc?: boolean }
+}
